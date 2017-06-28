eb886522c57e46e3aeeeca8c837a996ce7999be8
AshwinJd <jaydevashwin7@gmail.com>
2017-06-28 11:19:02 +0530

    Added production url and changed from ajax to superagent

--- getData.js
+++ getData.js
@@ -1,43 +1,45 @@
 const http = require('http');
-const request = require('ajax-request');
 const MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
+const request = require('superagent');
 
 //author: Ashwin Jayadev
 //api url with limit which is actually the no. of collections in the db at the particular timestamp
-let URL = 'https://internal.stackroute.in/api/v1/facade/reports/evaluations/1498674600/?q=before&limit=179';
+//Two urls URL_dev and URL_prod one for the development and pther production
+
+// let URL_dev = 'http://internal.stackroute.in/api/v1/facade/reports/evaluations/1498761000/?q=before&limit=179';
+let URL_prod = 'http://hobbes.stackroute.in/api/v1/facade/reports/evaluations/1498761000/?q=before&limit=391';
 
 // database name is analytics_Data
 const urlDb = 'mongodb://localhost:27017/analytics_Data';
 
 //author : Ashwin Jayadev
-// ajax request used to for get request
-
-request(URL, function(err, res, body) {});
+// superagent request used to for get request
 
 var insertDocuments = function(db, callback) {
-  let json;
-  request({
-    url: URL,
-    method: 'GET',
-   }, function(err, res, body) {
-    // json will have the entire collection in the form of array of objects
-    json = JSON.parse(body)["evaluations"];
-    // console.log(json);
-    var collection = db.collection('documents');
-    // Insert some documents
-    // console.log(json);
-    collection.insert(json, function(err, result) {
-      assert.equal(err, null);
-      // assert.equal(3, result.result.n);
-      // assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the document collection");
-      callback(result);
-    });
+
+    let json;
+    request
+      .get(URL_prod)
+      .end((err,res)=>{
+        if(err) throw err;
+        json = res.body["evaluations"];
 
 
-  });
-  // Get the documents collection
+        //  collection name is documents
+        let collection = db.collection('documents');
+        // Insert some documents
+        collection.insert(json, function(err, result) {
+          assert.equal(err, null);
+          // assert.equal(3, result.result.n);
+          // assert.equal(3, result.ops.length);
+          console.log("Inserted documents into the document collection");
+          callback(result);
+        });
+        // console.log(json);
+      })
+
+    // console.log(json);
   }
 
 //author: Ashwin Jayadev
